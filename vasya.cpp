2e1f84c0090437ed91afd2ae1d935a66b4637f31
dorakit <72117270+dorakit@users.noreply.github.com>
2020-09-30 08:45:55 +0530

    Create vasya.cpp
    Vasya has an array a consisting of positive integer numbers. Vasya wants to divide this array into two non-empty consecutive parts (the prefix and the suffix) so that the sum of all elements in the first part equals to the sum of elements in the second part. It is not always possible, so Vasya will move some element before dividing the array (Vasya will erase some element and insert it into an arbitrary position).
    
    Inserting an element in the same position he was erased from is also considered moving.
    
    Can Vasya divide the array after choosing the right element to move and its new position?
    
    Input
    The first line contains single integer n (1 ≤ n ≤ 100000) — the size of the array.
    
    The second line contains n integers a 1, a 2... a n (1 ≤ a i ≤ 109) — the elements of the array.
    
    Output
    Print YES if Vasya can divide the array after moving one element. Otherwise print NO.
    
    Examples
    inputCopy
    3
    1 3 2
    outputCopy
    YES
    inputCopy
    5
    1 2 3 4 5
    outputCopy
    NO
    inputCopy
    5
    2 2 3 4 5
    outputCopy
    YES
    Note
    In the first example Vasya can move the second element to the end of the array.
    
    In the second example no move can make the division possible.
    
    In the third example Vasya can move the fourth element by one position to the left.

---
+++ vasya.cpp
@@ -0,0 +1,43 @@
+
+#include<bits/stdc++.h>
+#include<map>
+#include<unordered_map>
+
+using namespace std;
+//http://codeforces.com/contest/808/problem/D
+int main()
+{
+/*map<char,int> M;
+unordered_map<char,int> U;
+
+string s="King Rishab Dugar";
+//add(key,value)--> log N (OM) ,O(1)(UOM)
+//erase(key) -->log N (OM) ,O(1)(UOM)
+
+for(char c : s) M[c]++; //O(NlogN) where N=|s| size of your string
+
+for(char c : s) U[c]++; //O(N) because Unordered maps can get and set values in just O(1) time*/
+int n;
+cin>>n;
+vector<int>A(n+5,0);//initailized with all 0s
+long long S=0;
+for(int i=0;i<n;i++) cin>>A[i],S+=A[i];
+//using unodered_map it speeds up solution
+map<long long,int> first,second;//We need two maps
+first[A[0]]=1;
+for(int i=1;i<n;i++) second[A[i]]++; // not=1 since array can have duplicates
+
+long long sdash=0;
+
+
+        else//if sdash is greater
+            {
+            long long y=sdash-S/2;
+            if(first[y]>0){
+                cout<<"YES\n";
+                return 0;
+            }
+       
+
+cout<<"NO\n";
+}
